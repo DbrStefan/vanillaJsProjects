17e9256710e3b257aba4c6b70327c069a7819964
DbrStefan <stefan.g.dobre@sidertrans.ro>
2020-10-29 15:30:19 +0200

    added two more mini apps

--- menuSlider-Modal/script.js
+++ menuSlider-Modal/script.js
@@ -2,31 +2,30 @@ const toggle = document.getElementById('toggle');
 const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
-const icon = document.getElementById('nav-icon4')
+const icon = document.getElementById('nav-icon4');
 const nav = document.getElementById('nav');
 
-toggle.addEventListener('click', ()=>{
-    icon.classList.toggle('open');
-})
+toggle.addEventListener('click', () => {
+  icon.classList.toggle('open');
+});
 
 // Toggle nav
-toggle.addEventListener('click', ()=>{
-    document.body.classList.toggle('show-nav')
-})
-
-
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+});
 
 // Show modal
-open.addEventListener('click', ()=>{
-    modal.classList.add('show-modal');
-})
+open.addEventListener('click', () => {
+  modal.classList.add('show-modal');
+});
 
 // Hide modal
 
-close.addEventListener('click', ()=>{
-    modal.classList.remove('show-modal');
+close.addEventListener('click', () => {
+  modal.classList.remove('show-modal');
 });
 
 // Hide modal on outside click
-window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
-
+window.addEventListener('click', (e) =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
